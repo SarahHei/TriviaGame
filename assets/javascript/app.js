b93c307779d76d22d4e486c5ca346bd31d326f35
SarahHei <sarahhei@compasseval.com>
2018-10-06 18:51:56 -0400

    Homework Update 2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,42 @@
 $(document).ready(function() {
 
 var questions;
-
-
-
 var timer = 30;
 var interval; 
 var correctResponses = 0; 
 var clickCounter = 0; 
 
 var questions = [
-    let question1 = "Who was the first known archaeologist?";
-    let q1PossibleAnswers = ["Howard Carter", "Hiram Bingham", "Herodotus", "William Cunnington"];
-    let q1CorrectAnswer = "Herodotus";
+    {
+        question1 = "Who was the first known archaeologist?";
+        q1PossibleAnswers = ["Howard Carter", "Hiram Bingham", "Herodotus", "William Cunnington"];
+        q1CorrectAnswer = "Herodotus";
+    },
     
-    let question2 = "Which of the following is NOT an archaeological field technique?"
-    let q2PossibleAnswers = ["Survey", "Lab Analysis", "LIDAR imaging", "Ground Penetrating Radar"];
-    let q2CorrectAnswer = "Lab Analysis";
-
-    let question3 = "Which of the following is a famous archaeological site?";
-    let q3PossibleAnswers = ["Eiffel Tower", "Vatican", "Berlin Opera House", "Petra"];
-    let q3CorrectAnswer = "Petra";
+    {
+        question2 = "Which of the following is NOT an archaeological field technique?"
+        q2PossibleAnswers = ["Survey", "Lab Analysis", "LIDAR imaging", "Ground Penetrating Radar"];
+        q2CorrectAnswer = "Lab Analysis";
+    },
     
-
-    let question4: "To what college or degree program is archaeology most closely linked?";
-    let q4PossibleAnswer = ["Anthropology", "Geology", "Statistics", "Biology"];
-    let q4CorrectAnswer = "Anthropology";
-
-    let question5: "Which of the following movies DOES NOT contain archaeology references?";
-    let q5PossibleAnswer = ["Indiana Jones and the Last Crusade", "Lara Croft: Tomb Raider", "The Mummy", "Mission Impossible"];
-    let q5CorrectAnswerr = "Mission Impossible";
+    {
+        question3 = "Which of the following is a famous archaeological site?";
+        q3PossibleAnswers = ["Eiffel Tower", "Vatican", "Berlin Opera House", "Petra"];
+        q3CorrectAnswer = "Petra";
+    },
+    
+    {
+        question4: "To what college or degree program is archaeology most closely linked?";
+        q4PossibleAnswers = ["Anthropology", "Geology", "Statistics", "Biology"];
+        q4CorrectAnswer = "Anthropology";
+    },
+    
+    {
+        question5: "Which of the following movies DOES NOT contain archaeology references?";
+        q5PossibleAnswers = ["Indiana Jones and the Last Crusade", "Lara Croft: Tomb Raider", "The Mummy", "Mission Impossible"];
+        q5CorrectAnswer = "Mission Impossible";
+    },
+    
 ];
 
 $("#start").on('click', function() {
@@ -38,15 +45,15 @@ $("#start").on('click', function() {
         if(timer === 0){
             timeOut();
         };
-        $("#questions").append("question1");
+        $("#questions").append("questions");
         console.log("questions");
     })
 
 function displayChoices(arr){
     for (var i = 0; i < arr.length; i++){
-      var answerDiv = $("<div class='choices'>");
-      answerDiv.text(arr[i]);
-      $("#questionChoices").append(answerDiv);
+      var possibleAnswersDiv = $("<div id="questionChoices">");
+      possibleAnswersDiv.text(arr[i]);
+      $("#questionChoices").append(possibleAnswersDiv);
     }
 } 
 
@@ -55,6 +62,5 @@ function stop() {
         questionProgress++;
         setTimeout(nextQuestion, 4000);
     }
-
 
 }
