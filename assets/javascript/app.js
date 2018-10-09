f6d8e2c8d14ca1c048161b033962b519ea21f7ea
SarahHei <sarahhei@compasseval.com>
2018-10-08 20:40:47 -0400

    Update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-var questions = [];
 var timer = 30;
 var intervalId; 
 var correctResponses = 0; 
@@ -23,44 +22,23 @@ function run() {
     clearInterval(intervalId);
   }
 
-  
-  // radio buttons
-  // submit function
-
-var questions = [
-    {
-        question1: "Who was the first known archaeologist?",
-        q1PossibleAnswers: ["Howard Carter", "Hiram Bingham", "Herodotus", "William Cunnington"],
-        q1CorrectAnswer:"Herodotus"
-    },
-    
-    {
-        question2: "Which of the following is NOT an archaeological field technique?",
-        q2PossibleAnswers: ["Survey", "Lab Analysis", "LIDAR imaging", "Ground Penetrating Radar"],
-        q2CorrectAnswer: "Lab Analysis"
-    },
-    
-    {
-        question3: "Which of the following is a famous archaeological site?",
-        q3PossibleAnswers: ["Eiffel Tower", "Vatican", "Berlin Opera House", "Petra"],
-        q3CorrectAnswer: "Petra"
-    },
-    
-    {
-        question4: "To what college or degree program is archaeology most closely linked?",
-        q4PossibleAnswers: ["Anthropology", "Geology", "Statistics", "Biology"],
-        q4CorrectAnswer: "Anthropology"
-    },
-    
-    {
-        question5: "Which of the following movies DOES NOT contain archaeology references?",
-        q5PossibleAnswers: ["Indiana Jones and the Last Crusade", "Lara Croft: Tomb Raider", "The Mummy", "Mission Impossible"],
-        q5CorrectAnswer: "Mission Impossible"
-    },
-    
-];
-
-// submit button click
+var questionArray = {
+        "question1": "Who was the first known archaeologist?",
+        "q1PossibleAnswers": ["Howard Carter", "Hiram Bingham", "Herodotus", "William Cunnington"],
+        "q1CorrectAnswer":"Herodotus",
+        "question2": "Which of the following is NOT an archaeological field technique?",
+        "q2PossibleAnswers": ["Survey", "Lab Analysis", "LIDAR imaging", "Ground Penetrating Radar"],
+        "q2CorrectAnswer": "Lab Analysis",
+        "question3": "Which of the following is a famous archaeological site?",
+        "q3PossibleAnswers": ["Eiffel Tower", "Vatican", "Berlin Opera House", "Petra"],
+        "q3CorrectAnswer": "Petra",
+        "question4": "To what college or degree program is archaeology most closely linked?",
+        "q4PossibleAnswers": ["Anthropology", "Geology", "Statistics", "Biology"],
+        "q4CorrectAnswer": "Anthropology",
+        "question5": "Which of the following movies DOES NOT contain archaeology references?",
+        "q5PossibleAnswers": ["Indiana Jones and the Last Crusade", "Lara Croft: Tomb Raider", "The Mummy", "Mission Impossible"],
+        "q5CorrectAnswer": "Mission Impossible"
+    }
 
 $("#start").on('click', function() {
     run();
@@ -70,12 +48,70 @@ $("#reset").on('click', function(){
     reset();
 });
 
-$("#questions").html(questions.question1);
-$("#option1").append(questions.q1PossibleAnswer[0]);
+    $("#questions").text("Your question is: " + questionArray.question1);
+    $("#option1").append(questionArray.q1PossibleAnswers[0]);
+    $("#option2").append(questionArray.q1PossibleAnswers[1]);
+    $("#option3").append(questionArray.q1PossibleAnswers[2]);
+    $("#option4").append(questionArray.q1PossibleAnswers[3]);
 
-if (questions.question1) {
-    q1PossibleAnswers===q1CorrectAnswers;
-    alert("That is correct!");
-  }
+    $("#option1").on("click", function () {
+        if (q1PossibleAnswers === q1CorrectAnswer) {
+            $("#questions").text("Your question is: " + questionArray.question2);
+            $("#option1").append(questionArray.q2PossibleAnswers);
+            $("#option2").append(questionArray.q2PossibleAnswers);
+            $("#option3").append(questionArray.q2PossibleAnswers);
+            $("#option4").append(questionArray.q2PossibleAnswers);
+            correctResponses++;
+        }
+        else {
+            alert("Try again!");
+            }
+    }
+
+    $("#option2").on("click", function () {
+        if (q2PossibleAnswers === q2CorrectAnswer) {
+            $("#questions").text("Your question is: " + questionArray.question3);
+            $("#option1").append(questionArray.q3PossibleAnswers);
+            $("#option2").append(questionArray.q3PossibleAnswers);
+            $("#option3").append(questionArray.q3PossibleAnswers);
+            $("#option4").append(questionArray.q3PossibleAnswers);
+            correctResponses++;
+        } else {
+            alert("Try again!");
+                }
+    }
+
+    $("#option3").on("click", function () {
+        if (q3PossibleAnswers === q3CorrectAnswer) {
+            $("#questions").text("Your question is: " + questionArray.question4);
+            $("#option1").append(questionArray.q4PossibleAnswers);
+            $("#option2").append(questionArray.q4PossibleAnswers);
+            $("#option3").append(questionArray.q4PossibleAnswers);
+            $("#option4").append(questionArray.q4PossibleAnswers);
+            correctResponses++;
+        } else {
+            alert("Try again!");
+                }
+    }
+
+    $("#option4").on("click", function () {
+        if (q4PossibleAnswers === q4CorrectAnswer) {
+            $("#questions").text("Your question is: " + questionArray.question5);
+            $("#option1").append(questionArray.q5PossibleAnswers);
+            $("#option2").append(questionArray.q5PossibleAnswers);
+            $("#option3").append(questionArray.q5PossibleAnswers);
+            $("#option4").append(questionArray.q5PossibleAnswers);
+            correctResponses++;
+        } else {
+            alert("Try again!");
+                }
+    }
+
+    if (q5PossibleAnswers === q5CorrectAnswer) {
+        correctResponses++;
+    }
+        else {
+            alert("Try again!");
+        }
 
 })
