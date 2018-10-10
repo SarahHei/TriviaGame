2bcf95ac8474a0045d44e9e2ced2399a7e081e5f
SarahHei <sarahhei@compasseval.com>
2018-10-10 19:13:50 -0400

    Update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 
 var timer = 30;
 var intervalId; 
-var correctResponses = 0; 
-
+var correctResponses = 0;
 function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
   }
 
+//this section sets up the timer
 function decrement() {
     timer--;
     $(".countdown").html("<p>" + timer + "<p>");
@@ -21,7 +21,8 @@ function decrement() {
 function stop() {
     clearInterval(intervalId);
   }
-  
+
+//start and reset buttons
 $("#start").on('click', function() {
     run();
     })
@@ -30,46 +31,105 @@ $("#reset").on('click', function(){
     run();
 })
 
-var questionArray = {
-        "question1": "Who was the first known archaeologist?",
-        "q1PossibleAnswers": ["Howard Carter", "Hiram Bingham", "Herodotus", "William Cunnington"],
-        "q1CorrectAnswer":"Herodotus",
-        "question2": "Which of the following is NOT an archaeological field technique?",
-        "q2PossibleAnswers": ["Survey", "Lab Analysis", "LIDAR imaging", "Ground Penetrating Radar"],
-        "q2CorrectAnswer": "Lab Analysis",
-        "question3": "Which of the following is a famous archaeological site?",
-        "q3PossibleAnswers": ["Eiffel Tower", "Vatican", "Berlin Opera House", "Petra"],
-        "q3CorrectAnswer": "Petra",
-        "question4": "To what college or degree program is archaeology most closely linked?",
-        "q4PossibleAnswers": ["Anthropology", "Geology", "Statistics", "Biology"],
-        "q4CorrectAnswer": "Anthropology",
-        "question5": "Which of the following movies DOES NOT contain archaeology references?",
-        "q5PossibleAnswers": ["Indiana Jones and the Last Crusade", "Lara Croft: Tomb Raider", "The Mummy", "Mission Impossible"],
-        "q5CorrectAnswer": "Mission Impossible"
+//this is the array containing 5 objects; each object contains a question, four possible answers, and a correct answer
+//position i in the array refers to each object; thus [0] is object 1, etc.
+//each object contains an array for possible answers; position [j] refers to items in this array
+var questionArray = [
+    {
+        question: "Who was the first known archaeologist?",
+        possibleAnswers: ["Howard Carter", "Hiram Bingham", "Herodotus", "William Cunnington"],
+        correctAnswer:"Herodotus"
+    },
+    {
+        question: "Which of the following is NOT an archaeological field technique?",
+        possibleAnswers: ["Survey", "Lab Analysis", "LIDAR imaging", "Ground Penetrating Radar"],
+        correctAnswer: "Lab Analysis"
+    },
+    {
+        question: "Which of the following is a famous archaeological site?",
+        possibleAnswers: ["Eiffel Tower", "Vatican", "Berlin Opera House", "Petra"],
+        correctAnswer: "Petra"
+    }, 
+    { 
+        question: "To what college or degree program is archaeology most closely linked?",
+        possibleAnswers: ["Anthropology", "Geology", "Statistics", "Biology"],
+        correctAnswer: "Anthropology"
+    },
+    {
+        question: "Which of the following movies DOES NOT contain archaeology references?",
+        possibleAnswers: ["Indiana Jones and the Last Crusade", "Lara Croft: Tomb Raider", "The Mummy", "Mission Impossible"],
+        correctAnswer: "Mission Impossible"
     }
+]
 
+var showMeQuestions = function() {
+    for (var i = 0; i <5; i++) {
+        $("#questions").text("Your question is: " + questionArray[i].question);
+        $("#radioButton1").text(questionArray[i].possibleAnswers[0]);
+        $("#radioButton2").text(questionArray[i].possibleAnswers[1]);
+        $("#radioButton3").text(questionArray[i].possibleAnswers[2]);
+        $("#radioButton4").text(questionArray[i].possibleAnswers[3]);
+    }
+    
+/*
+    for (var j = 0; j<= 3; j++) {
+        
+        }  
+*/   
+}
+    
+for (var j = 0; j < 5; j++) {
+    if ($("#radioButton").questionArray[i].possibleAnswers[j].checked) === questionArray[i].correctAnswer {
+        correctResponses++;
+    } else {
+        alert("Sorry, try again!");
+    }
+    }
+})
 
-
-    $("#questions").text("Your question is: " + questionArray.question1);
+/*
+$("#questions").text("Your question is: " + questionArray.question1);
     let option1 = questionArray.q1PossibleAnswers[0];
-    $("#option1").append(questionArray.q1PossibleAnswers[0]);
+    $("#option1").text(questionArray.q1PossibleAnswers[0]);
     let option2 = questionArray.q1PossibleAnswers[1];
-    $("#option2").append(questionArray.q1PossibleAnswers[1]);
+    $("#option2").text(questionArray.q1PossibleAnswers[1]);
     let option3 = questionArray.q1PossibleAnswers[2];
-    $("#option3").append(questionArray.q1PossibleAnswers[2]);
+    $("#option3").text(questionArray.q1PossibleAnswers[2]);
     let option4 = questionArray.q1PossibleAnswers[3];
-    $("#option4").append(questionArray.q1PossibleAnswers[3]);
+    $("#option4").text(questionArray.q1PossibleAnswers[3]);
     
     $("#option1").on("click", function () {
        if (option1 === questionArray.q1CorrectAnswer) {
             $("#questions").text("Your question is: " + questionArray.question2);
-            $("#option1").append(questionArray.q2PossibleAnswers[0]);
+            console.log(option1);
+            $("#option1").text(questionArray.q2PossibleAnswers[0]);
             let option1 = questionArray.q2PossibleAnswers[0];
-            $("#option2").append(questionArray.q2PossibleAnswers[1]);
+            $("#option2").text(questionArray.q2PossibleAnswers[1]);
             let option2 = questionArray.q2PossibleAnswers[1];
-            $("#option3").append(questionArray.q2PossibleAnswers[2]);
+            $("#option3").text(questionArray.q2PossibleAnswers[2]);
             let option3 = questionArray.q2PossibleAnswers[2];
-            $("#option4").append(questionArray.q2PossibleAnswers[3]);
+            $("#option4").text(questionArray.q2PossibleAnswers[3]);
+            let option4 = questionArray.q2PossibleAnswers[3];
+            correctResponses++;
+            $("#correctResponses").text(correctResponses);
+        }
+        else {
+            alert("Try again!");
+            console.log(this);
+            }
+        })
+
+   $("#option2").on("click", function () {
+       if (option2 === questionArray.q1CorrectAnswer) {
+            $("#questions").text("Your question is: " + questionArray.question2);
+            console.log(option1);
+            $("#option1").text(questionArray.q2PossibleAnswers[0]);
+            let option1 = questionArray.q2PossibleAnswers[0];
+            $("#option2").text(questionArray.q2PossibleAnswers[1]);
+            let option2 = questionArray.q2PossibleAnswers[1];
+            $("#option3").text(questionArray.q2PossibleAnswers[2]);
+            let option3 = questionArray.q2PossibleAnswers[2];
+            $("#option4").text(questionArray.q2PossibleAnswers[3]);
             let option4 = questionArray.q2PossibleAnswers[3];
             correctResponses++;
             $("#correctResponses").text(correctResponses);
@@ -78,22 +138,9 @@ var questionArray = {
             alert("Try again!");
             }
         })
-    
-   $("#option2").on("click", function () {
-        if (questionArray.q2PossibleAnswers === questionArray.q2CorrectAnswer) {
-            $("#questions").text("Your question is: " + questionArray.question3);
-            $("#option1").append(questionArray.q3PossibleAnswers[0]);
-            $("#option2").append(questionArray.q3PossibleAnswers[1]);
-            $("#option3").append(questionArray.q3PossibleAnswers[2]);
-            $("#option4").append(questionArray.q3PossibleAnswers[3]);
-            correctResponses++;
-        } else {
-            alert("Try again!");
-                }
-    })
 
     $("#option3").on("click", function () {
-        if (questionArray.q3PossibleAnswers === questionArray.q3CorrectAnswer) {
+        if (option3 === questionArray.q3CorrectAnswer) {
             $("#questions").text("Your question is: " + questionArray.question4);
             $("#option1").append(questionArray.q4PossibleAnswers[0]);
             $("#option2").append(questionArray.q4PossibleAnswers[1]);
@@ -124,4 +171,6 @@ var questionArray = {
         else {
             alert("Try again!");
         }
-})
+*/
+
+
